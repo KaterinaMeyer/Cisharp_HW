feaf2c569b798e3ca18d9305ddee3c0eeef12f61
Катерина <katttum@ya.ru>
2023-06-24 21:53:14 +0300

    максимальное из 3-х

---
+++ Exp002_max3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c) 
+{
+    Console.WriteLine("Максимальное число = " + a);
+}
+else if (b > a && b > c) 
+{
+    Console.WriteLine("Максимальное число = " + b);
+}
+else {
+    Console.WriteLine("Максимальное число = " + c);
+}
+
