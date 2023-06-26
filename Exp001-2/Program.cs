25a6ee1db9d7f61b57df4fe221c478e59b701036
Катерина <katttum@ya.ru>
2023-06-26 10:36:43 +0300

    git ignore

---
+++ Exp001-2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int num = new Random().Next(100, 1000);
+int num1 = num / 100;
+int num2 = num % 10;
+Console.WriteLine($"число задумали {num}");
+Console.WriteLine($"число вывели {num1}{num2}");
--- Exp001-2/obj/Debug/net6.0/Exp001-2.AssemblyInfo.cs
+++ Exp001-2/obj/Debug/net6.0/Exp001-2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("sem1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Exp001-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("sem1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("sem1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Exp001-2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Exp001-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
