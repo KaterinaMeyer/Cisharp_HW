5a292b0844787f04e6869fa46a3962a81579c450
Катерина <katttum@ya.ru>
2023-06-26 13:52:55 +0300

    кратность вводимых чисел

---
+++ Exp2_002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. Если второе число не кратно числу первому,то программа выводит остаток от деления.
+
+Console.Clear();
+
+Console.Write("Введите число 1 -> ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2 -> ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1 % num2;
+
+if (result == 0)
+{
+    Console.Write($"Число {num1} кратно числу {num2}");
+}
+else
+{
+    Console.Write($"Число {num1} не кратно числу {num2}, остаток равен {result}");
+}
+
