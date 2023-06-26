1ecb6e1bd10b54b19082b46e93ef8ff94aa4fbd8
Катерина <katttum@ya.ru>
2023-06-26 13:55:08 +0300

    кратность вводимых чисел 23 и 7

---
+++ Exp003-2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вводимое число кратно одновременно 23 и 7
+
+
+Console.Clear();
+Console.Write("Введите число 1 -> ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if ((num1%7 == 0)&&(num1%23 == 0))
+{
+    Console.Write($"Наше число {num1} кратно 7 и 23");
+}
+else
+{
+    Console.Write($"Наше число {num1} не кратно 7 и 23");
+}
+
