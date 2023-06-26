2c8953297a8d01268408c23ae2c93c81c4325b63
Катерина <katttum@ya.ru>
2023-06-26 13:49:44 +0300

    удалить среднюю цифру из 3 зн числа

---
+++ Exp2_001/Program.cs
@@ -0,0 +1,10 @@
+﻿﻿// Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+
+
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int num1 = num / 100;
+int num2 = num % 10;
+Console.WriteLine($"число задумали {num}");
+Console.WriteLine($"число вывели {num1}{num2}");
