2cf13876b4219735cc48366dbe9d66ec14dc1708
Катерина <katttum@ya.ru>
2023-06-26 13:45:58 +0300

    квадрат числа второе число

---
+++ Exp002-2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите число 1 -> ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2 -> ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1 % num2;
+
+if (result == 0)
+{
+    Console.Write($"Число {num1} кратно числу {num2}");
+}
+else
+{
+    Console.Write($"Число {num1} не кратно числу {num2}, остаток равен {result}");
+}
+
